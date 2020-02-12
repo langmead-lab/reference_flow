4562b87d922539e4c306598f7af9d950cc5e0572
milkschen <s93rm6@gmail.com>
2020-02-11 23:33:02 -0500

    script for downloading grch38

---
+++ src/download_genome.sh
@@ -0,0 +1,6 @@
+# This script downloads GRCh38 reference genome from NCBI
+
+cd resources/
+wget ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/000/001/405/GCA_000001405.15_GRCh38/seqs_for_alignment_pipelines.ucsc_ids/GCA_000001405.15_GRCh38_no_alt_analysis_set.fna.gz
+bgzip -d GCA_000001405.15_GRCh38_no_alt_analysis_set.fna.gz
+cd ../
