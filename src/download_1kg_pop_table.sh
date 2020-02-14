1abde6b3613b4f2e8b2e4c7d27c1e01df65ef60d
milkschen <s93rm6@gmail.com>
2020-02-14 15:57:45 -0500

    simplification

--- src/download_1kg_pop_table.sh
+++ src/download_1kg_pop_table.sh
@@ -1,3 +1,1 @@
-cd resources
-wget ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/technical/working/20130606_sample_info/20130606_g1k.ped
-cd ../
+wget -P resources/ ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/technical/working/20130606_sample_info/20130606_g1k.ped
