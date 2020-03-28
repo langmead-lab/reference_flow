9ff0c406f90cd7183f143de8a50cb6cb61c4f07f
milkschen <s93rm6@gmail.com>
2020-03-28 15:04:44 -0400

    wrote more detailed documentation; refactored; added several assertions to make sure each SAM input share the same set of reads

--- src/merge_incremental.py
+++ src/merge_incremental.py
@@ -24,26 +24,19 @@ def parse_args():
         '-p', '--prefix',
         help='prefix of the merged files'
     )
-    # parser.add_argument(
-    #     '-id', '--id',
-    #     help='name of new file to be merged'
-    # )
     args = parser.parse_args()
     return args
 
-args = parse_args()
-fn_sam = args.sam_list # 'to_merge.path'
-fn_ids = args.id_list # 'to_merge.id'
-fn_log = args.log # 'merged.path'
-# new_id = args.id # 'EUR'
-prefix = args.prefix # 'test'
-
-#: set random seed
-seed = args.rand_seed
-sys.stderr.write('Set random seed: {}\n'.format(seed))
-random.seed(seed)
-
 def get_name_as_mapq(line):
+    '''
+    Read one line from SAM and return QNAME, alignment score (AS:i) and MAPQ
+
+    Args:
+        a line from SAM
+
+    Returns:
+        QNAME, AS:i, MAPQ
+    '''
     if line[0] == '@':
         return 'header', None
     line = line.split()
@@ -62,6 +55,20 @@ def read_line(istream):
             return line
 
 def compare_score_and_mapq(list_info):
+    '''
+    Select the best record given a number of alignment results.
+    Sorting criteria:
+        1. aligned > un-aligned
+        2. take higher alignment score
+        3. take higher mapping quality
+        4. if all the above are tied, pick one randomly
+
+    Args:
+        a list of alignment infos
+
+    Returns:
+        the best record from the provided list
+    '''
     order = list(range(len(list_info)))
     list_is_unaligned = [info[0] != 1 for info in list_info]
     list_as = [info[0] for info in list_info]
@@ -92,60 +99,94 @@ def compare_score_and_mapq(list_info):
     return random.sample(order, 1)
 
 def get_best_line(list_line):
+    '''
+    Given a number of SAM lines, select the best one.
+
+    Args:
+        a list of SAM lines
+    
+    Returns:
+        the index of the best line, and the line itself
+    '''
     list_info = []
     list_name = []
     for line in list_line:
         info = get_name_as_mapq(line)
         list_name.append(info[0])
-        assert info[0] == list_name[0]
+        try:
+            assert info[0] == list_name[0]
+        except:
+            print (info)
+            print (list_name)
+            exit (1)
         list_info.append(info[1:])
     idx = compare_score_and_mapq(list_info)[0]
     return idx, list_line[idx]
 
-with open(fn_sam, 'r') as f:
-    list_fn_sam = [] #: list of SAM names
-    list_f_sam = [] #: list of opened SAM files
-    for line in f:
-        fn_sam = line.rstrip()
-        list_fn_sam.append(fn_sam)
-        list_f_sam.append(open(fn_sam, 'r'))
-with open(fn_ids, 'r') as f:
-    list_ids = []
-    for line in f:
-        list_ids.append(line.rstrip())
-    # list_ids.append(new_id)
+if __name__ == '__main__':
+    args = parse_args()
+    fn_sam = args.sam_list # 'to_merge.path'
+    fn_ids = args.id_list # 'to_merge.id'
+    fn_log = args.log # 'merged.path'
+    prefix = args.prefix
+    
+    #: set random seed
+    seed = args.rand_seed
+    sys.stderr.write('Set random seed: {}\n'.format(seed))
+    random.seed(seed)
+    
+    with open(fn_sam, 'r') as f:
+        list_fn_sam = [] #: list of SAM names
+        list_f_sam = [] #: list of opened SAM files
+        for line in f:
+            fn_sam = line.rstrip()
+            list_fn_sam.append(fn_sam)
+            list_f_sam.append(open(fn_sam, 'r'))
+    with open(fn_ids, 'r') as f:
+        list_ids = []
+        for line in f:
+            list_ids.append(line.rstrip())
 
-if fn_log != None:
-    f_log = open(fn_log, 'w')
-list_f_out = []
-sys.stderr.write('output files: \n')
-for i in range(len(list_ids)):
-    fn_out = prefix + '-' + list_ids[i] + '.sam'
+    # number of files should match number of labels
+    len_list = len(list_fn_sam)
+    try:
+        assert len_list == len(list_ids)
+    except:
+        print ('Error: numbers of files and labels do not match')
+        exit (1)
+    
+    
     if fn_log != None:
-        f_log.write(fn_out + '\n')
-    sys.stderr.write(fn_out + '\n')
-    list_f_out.append(open(fn_out, 'w'))
-
-for line in list_f_sam[0]:
-    if line[0] == '@':
-        list_f_out[0].write(line)
-        continue
-    list_read = [line]
-    for i, f in enumerate(list_f_sam[1:]):
-        f_line = f.readline()
-        while f_line[0] == '@':
-            list_f_out[i+1].write(f_line)
+        f_log = open(fn_log, 'w')
+    list_f_out = []
+    sys.stderr.write('output files: \n')
+    for i in range(len(list_ids)):
+        fn_out = prefix + '-' + list_ids[i] + '.sam'
+        if fn_log != None:
+            f_log.write(fn_out + '\n')
+        sys.stderr.write(fn_out + '\n')
+        list_f_out.append(open(fn_out, 'w'))
+    
+    for line in list_f_sam[0]:
+        if line[0] == '@':
+            list_f_out[0].write(line)
+            continue
+        list_read = [line]
+        for i, f in enumerate(list_f_sam[1:]):
             f_line = f.readline()
-        # f_line = read_line(f, i)
-        list_read.append(f_line)
-    best_idx, best_line = get_best_line(list_read)
-    list_f_out[best_idx].write(best_line)
+            while f_line[0] == '@':
+                list_f_out[i+1].write(f_line)
+                f_line = f.readline()
+            # f_line = read_line(f, i)
+            list_read.append(f_line)
 
-# for line in sys.stdin:
-#     if line[0] == '@':
-#         continue
-#     list_read = [line]
-#     for f in list_f_sam:
-#         list_read.append(read_line(f))
-#     best_idx, best_line = get_best_line(list_read)
-#     list_f_out[best_idx].write(best_line)
+        # each read should be included by all SAM files
+        try:
+            assert len(list_read) == len_list
+        except:
+            print ('Error: number of alignments does not match')
+            print (list_read)
+            exit (1)
+        best_idx, best_line = get_best_line(list_read)
+        list_f_out[best_idx].write(best_line)
+    
